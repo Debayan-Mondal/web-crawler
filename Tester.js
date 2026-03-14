2e84dc7128ccc2792885ae7bf375be3f7a06952e
Debayan <debayanwb17@gmail.com>
2026-03-14 20:13:06 +0530

    Removing Tester.js

--- Tester.js
+++
@@ -1,8 +0,0 @@
-import { getHTMLfromPage, normalizeURL, divideURL } from "./Spider.js"
-
-(() => {
-    const url = ["https://www.wagslane.dev/tags/", "https://www.google.com","https://www.youtube.com", "asd","asdasdadsad"];
-    divideURL(url);
-   
-
-})()
